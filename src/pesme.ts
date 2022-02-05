ab94e7caee1f11e66bf6b1c967d4ca80c0ce75bd
Predrag Stojadinovic <predrags@nvidia.com>
2022-02-05 21:19:26 +0100

    wip

--- src/pesme.ts
+++ src/pesme.ts
@@ -10,23 +10,38 @@ const extractor = new WordExtractor();
 
 const _isUpper = (t: string) => t === _.toUpper(t);
 
-const extract = (folder: string, wordfile: string) => {
-	const extracted = extractor.extract(path.join(folder, `${wordfile}`));
+const extract = (folder: string, wordfile: string): void => {
+	const ext = path.extname(wordfile);
+	if (ext !== '.docx') return;
 
+	if (!fs.existsSync(path.join(folder, 'pesme'))) fs.mkdirSync(path.join(folder, 'pesme'));
+
+	const extracted = extractor.extract(path.join(folder, `${wordfile}`));
 	extracted.then((doc: any) => {
 		let body = doc.getBody();
 		body = body.replace(/\n\s*\n/g, '\n\n');
 		let lines: any[] = _.split(body, '\n');
 		lines = _.map(lines, (line) => _.replace(line, '&', ''));
 		lines = _.map(lines, (line) => _.replace(line, '*', ''));
+		lines = _.map(lines, (line) => _.replace(line, '*', ''));
+		lines = _.map(lines, (line) => _.replace(line, '*', ''));
 		lines = _.map(lines, (line) => _.replace(line, '#', ''));
 		lines = _.map(lines, _.trim);
 		lines = _.map(lines, (line: string) => {
+			if (_.startsWith(line, '---')) return '';
+			if (_.includes(line, '....')) return '';
+			if (_.includes(line, '\t')) return '';
+
 			if (line === '*') return '';
 			if (line === '#') return '';
 			if (line === '.') return '';
 			if (line === ',') return '';
 			if (_.startsWith(line, '?')) line = _.replace(line, '?', '');
+			if (_.startsWith(line, '?')) line = _.replace(line, '?', '');
+			if (_.startsWith(line, '+')) line = _.replace(line, '+', '');
+			if (_.startsWith(line, '+')) line = _.replace(line, '+', '');
+			if (_.startsWith(line, '%')) line = _.replace(line, '%', '');
+
 			return line;
 		});
 
@@ -36,27 +51,47 @@ const extract = (folder: string, wordfile: string) => {
 
 		let pesme: any[] = [];
 		let current: any = {title: '', text: []};
-		_.each(lines, (line: string) => {
+		for (let line of lines) {
 			if (!_.isEmpty(line) && _isUpper(line)) {
 				if (current?.title) pesme.push(current);
 				current = {title: line, text: []};
 			} else if (!!current) current?.text.push(line);
-		});
+		}
 
 		pesme = _.sortBy(pesme, 'title');
-		fs.writeFileSync(path.join(__dirname, `pesme.json`), JSON.stringify(pesme, null, 4));
-
-		_.each(pesme, (pesma: any) => {
+		for (const pesma of pesme) {
 			let filename = _.replace(pesma.title, '?', '');
+			filename = _.replace(filename, '%', '');
+			filename = _.replace(filename, '"', '');
+			filename = _.replace(filename, '"', '');
+			filename = _.replace(filename, '”', '');
+			filename = _.replace(filename, '”', '');
 			const text = pesma.title + '\n\n' + _.trim(_.join(pesma.text, '\n'));
 
-			let newFile = path.join(__dirname, `pesme/${filename} --- (${wordfile}).txt`);
+			let newFile = path.join(folder, `pesme/${filename} --- (${wordfile}).txt`);
 			if (fs.existsSync(newFile)) newFile = increment(newFile, {platform: 'win32'});
 
 			fs.writeFileSync(newFile, text);
-		});
+			// console.log(folder, wordfile, 'allFiles.push', path.basename(newFile));
+		}
 	});
 };
 
-const file = 'ГЛАВНО СТЕПЕНИШТЕ.docx';
-extract(__dirname, file);
+const wordovi = path.join(__dirname, 'wordovi');
+const subfolderNames = fs.readdirSync(wordovi);
+const subfolders = _.filter(subfolderNames, (sub: string) => fs.lstatSync(path.join(wordovi, sub)).isDirectory());
+_.each(subfolders, (subfolder) => {
+	const fullPath = path.join(wordovi, subfolder);
+
+	const fileNames = fs.readdirSync(fullPath);
+	const files = _.filter(fileNames, (file: string) => !fs.lstatSync(path.join(fullPath, file)).isDirectory());
+
+	for (const file of files) {
+		extract(fullPath, file);
+	}
+
+	let spisak: string[] = fs.readdirSync(path.join(fullPath, 'pesme'));
+	spisak = _.sortBy(spisak);
+	console.log(subfolder, 'spisak', _.size(spisak));
+	fs.writeFileSync(path.join(__dirname, `${subfolder}-spisak.txt`), _.join(spisak, '\n'));
+});
